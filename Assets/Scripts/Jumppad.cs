ecab9e552f0fa9c030e56569d4cb3a379ddff439
Leo Cook <107010131+Raumfist2@users.noreply.github.com>
2023-08-29 00:02:31 +1200

    Models

--- Assets/Scripts/Jumppad.cs
+++ Assets/Scripts/Jumppad.cs
@@ -6,11 +6,13 @@ public class Jumppad : MonoBehaviour
 {
     [Range(100, 10000)]
     public float bounceheight;
+    public AudioSource audioSource;
 
     private void OnCollisionEnter(Collision collision) 
     {
         GameObject bouncer = collision.gameObject;
         Rigidbody rb = bouncer.GetComponent<Rigidbody>();
         rb.AddForce(Vector3.up * bounceheight);
+        audioSource.Play();
     }
 }
